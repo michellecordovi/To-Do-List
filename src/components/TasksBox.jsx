6b5f3bd4231293b57d8efb8c4b1ec228bfd52cc2
MichelleCordovi <michellecordovi@gmail.com>
2024-09-21 14:25:07 -0400

    create tasks grid box

--- src/components/TasksBox.jsx
+++ src/components/TasksBox.jsx
@@ -1,9 +1,11 @@
 import InputBox from "./InputBox";
+import TasksGrid from "./TasksGrid";
 
 function TasksBox(){
     return (
         <section id="to-do-section">
             <InputBox />
+            <TasksGrid />
         </section>
     )
 }
---
+++ src/components/TasksGrid.jsx
@@ -0,0 +1,23 @@
+function TasksGrid() {
+    return (
+        <div id="tasks-grid">
+            <div id="to-do-list">
+
+            </div>
+
+            <div id="task-filters">
+                <span id="items-left">0 Items left</span>
+
+                <div id="filter-options">
+                    <span>All</span>
+                    <span>Active</span>
+                    <span>Complete</span>
+                </div>
+
+                <span id="clear-completed">Clear Complete</span>
+            </div>
+        </div>
+    )
+}
+
+export default TasksGrid;
