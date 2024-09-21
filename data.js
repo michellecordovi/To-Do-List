159d2197eeb1a13bd37e05da0e42b13eadf9a91c
MichelleCordovi <michellecordovi@gmail.com>
2024-09-21 16:11:34 -0400

    add font size and color changes

--- data.js
+++ data.js
@@ -2,6 +2,10 @@ const tasks = [
     {
       description:"Walk the dog",
       completed: false  
+    },
+    {
+        description: "Feed the dogs",
+        completed: false
     }
 ]
 
--- src/components/TasksGrid.jsx
+++ src/components/TasksGrid.jsx
@@ -2,35 +2,36 @@ import Task from "./Task";
 import tasks from "../../data";
 
 function TasksGrid() {
+    
 
 
     return (
-        <div id="tasks-grid">
-            <h2>Your tasks: </h2>
+		<div id="tasks-grid">
+			<h2>Your tasks: </h2>
 
-            <div id="to-do-list">
-                {tasks.map(task => (
-                    <Task 
-                        key={task.description} 
-                        description={task.description} 
-                        competed={task.completed} 
-                    />
-                ))}
-            </div>
+			<div id="to-do-list">
+				{tasks.map((task) => (
+					<Task
+						key={task.description}
+						description={task.description}
+						competed={task.completed}
+					/>
+				))}
+			</div>
 
-            <div id="task-filters">
-                <span id="items-left">0 Items left</span>
+			<div id="task-filters">
+				<span id="items-left">{tasks.length} items left</span>
 
-                <div id="filter-options">
-                    <span>All</span>
-                    <span>Active</span>
-                    <span>Complete</span>
-                </div>
+				<div id="filter-options">
+					<span className="filter-option">All</span>
+					<span className="filter-option">Active</span>
+					<span className="filter-option">Complete</span>
+				</div>
 
-                <span id="clear-completed">Clear Complete</span>
-            </div>
-        </div>
-    )
+				<span id="clear-completed">Clear Complete</span>
+			</div>
+		</div>
+	);
 }
 
 export default TasksGrid;
