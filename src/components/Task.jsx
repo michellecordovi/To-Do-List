ec182de8cbd2b3f0ad02ddc7c09723d01679f707
MichelleCordovi <michellecordovi@gmail.com>
2024-09-22 11:51:45 -0400

    create logic for completing and uncompleting tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,58 @@
 /* eslint-disable react/prop-types */
-function Task({description}) {
+import tasks from "../../data";
+import { useState } from "react";
+
+function Task({description, completedTasks, setCompletedTasks}) {
+    const index = tasks.findIndex(item => item.description === description); //finds index of this particular tasks in the tasks data
+
+    const [isComplete, setIsComplete] = useState(false);
+
+	//This will show checkmark on a task when clicked and update tasks data
+	function checkTask() {
+        
+        setIsComplete(!isComplete);
+
+        tasks[index].completed = isComplete;//changes the value of the tasks 'completed' data when clicked
+        //sets is completed state
+
+        //pushes or splices this task from the completed tasks array to calculate how many tasks are left
+        if(completedTasks.includes(description)){
+            setCompletedTasks(completedTasks.filter(task => task !== description))
+        } else {
+            setCompletedTasks([...completedTasks, description])
+        }
+	}
+
     return (
-        <div className="task">
-            <div className = "task-checkbox"></div>
-            <p className="task-description">{description}</p>
-        </div>
-    )
+		<div className="task">
+			<div
+				onClick={checkTask}
+				className={
+					isComplete
+						? "task-checkbox is-checked"
+						: "task-checkbox"
+				}
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+					<path
+						fill="none"
+						stroke="#FFF"
+						strokeWidth="2"
+						d="M1 4.304L3.696 7l6-6"
+					/>
+				</svg>
+			</div>
+			<p
+				className={
+					isComplete
+						? "task-description completed-task"
+						: "task-description"
+				}
+			>
+				{description}
+			</p>
+		</div>
+	);
 }
 
 export default Task;
--- src/components/TasksGrid.jsx
+++ src/components/TasksGrid.jsx
@@ -1,26 +1,34 @@
 import Task from "./Task";
 import tasks from "../../data";
+import {useState, useEffect} from 'react'
 
 function TasksGrid() {
-    
+    const [completedTasks, setCompletedTasks] = useState([])
+    const [tasksLeft, setTasksLeft] = useState(tasks.length);
 
+    useEffect(() => {
+       setTasksLeft(() => {
+            return tasks.length - completedTasks.length
+       })
+    }, [completedTasks])
 
     return (
 		<div id="tasks-grid">
 			<h2>Your tasks: </h2>
 
 			<div id="to-do-list">
-				{tasks.map((task) => (
+				{tasks.map((task, index) => (
 					<Task
-						key={task.description}
+						key={index}
 						description={task.description}
-						competed={task.completed}
+                        completedTasks={completedTasks}
+                        setCompletedTasks={setCompletedTasks}
 					/>
 				))}
 			</div>
 
 			<div id="task-filters">
-				<span id="items-left">{tasks.length} items left</span>
+				<span id="items-left">{tasksLeft} items left</span>
 
 				<div id="filter-options">
 					<span className="filter-option">All</span>
