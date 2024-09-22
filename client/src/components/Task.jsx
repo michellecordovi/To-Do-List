90879eaa3f75d906ebe5c0f7021f54706663d064
MichelleCordovi <michellecordovi@gmail.com>
2024-09-22 17:25:31 -0400

    add margins to main and max height to todo list with scroll overflow

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -40,6 +40,7 @@ function Task({ tasks, description, completedTasks, setCompletedTasks }) {
 					/>
 				</svg>
 			</div>
+
 			<p
 				className={
 					isComplete
@@ -49,6 +50,16 @@ function Task({ tasks, description, completedTasks, setCompletedTasks }) {
 			>
 				{description}
 			</p>
+
+			<div className="delete-button">
+				<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+					<path
+						fill="#494C6B"
+						fillRule="evenodd"
+						d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+					/>
+				</svg>
+			</div>
 		</div>
 	);
 }
